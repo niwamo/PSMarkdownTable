0109e9ebbdda61bf0eb4bde03bd0b69b0ac21efc
niwamo <nm@niwamo.com>
2023-09-09 21:14:05 -0500

    improved pipeline handling

--- PSMarkdownTable.psm1
+++ PSMarkdownTable.psm1
@@ -6,52 +6,68 @@ function ConvertFrom-MdTable {
         )]$InputObject
     )
 
-    # get fields from each row
-    $rows = @()
-    foreach ($row in $InputObject -split "\n") {
-        $innerArray = @()
-        $fields = $row.trim("|").split("|") | ForEach-Object {$_.trim()}
-        foreach ($field in $fields) {
-            $innerArray += $field
+    begin {
+        # initialize empty generic list
+        $all = @()
+    }
+  
+    process {
+        # add incoming pipeline object to list:
+        foreach($input in $InputObject) {
+            $all += $input
         }
-        $rows += @(,$innerArray)
     }
 
-    # make sure all rows have the same number of fields
-    $uniformLength = $true
-    $numUniqueLengths = ($rows | ForEach-Object {$_.length} | Select-Object -Unique | Measure-Object).count
-    if ($numUniqueLengths -gt 1) {
-        $uniformLength = $false
-    }
-
-    # make sure there's a 'separator' row between the table header and the rows
-    $headerSeparator = $true
-    $divider = [string]::Join('',$rows[1])
-    if (($divider.toCharArray() | Get-Unique) -ne "-") {
-        $headerSeparator = $false
-    }
-
-    # input validation
-    if (!($uniformLength -and $headerSeparator)) {
-        Write-Output "Invalid input object"
-        return
-    }
-
-    # Process into array of hashtables
-    $headers = $rows[0]
-    $entries = $rows[2..($rows.length - 1)]
-    $output = @()
-
-    foreach ($row in $entries) {
-        $rowArray = $row.trim("|").split("|")
-        $rowOutput = @{}
-        foreach ($idx in 0..($rowArray.length - 1)) {
-            $rowOutput.add($headers[$idx], $rowArray[$idx].trim())
+    end {
+        # get fields from each row
+        $rows = @()
+        foreach ($row in $all -split "`n") {
+            $innerArray = @()
+            $fields = $row.trim("|").split("|") | ForEach-Object {$_.trim()}
+            foreach ($field in $fields) {
+                $innerArray += $field
+            }
+            $rows += @(,$innerArray)
         }
-        $output = $output + $rowOutput
-    }
 
-    return $output
+        # make sure all rows have the same number of fields
+        $uniformLength = $true
+        $numUniqueLengths = ($rows | ForEach-Object {$_.length} | Select-Object -Unique | Measure-Object).count
+        if ($numUniqueLengths -gt 1) {
+            $uniformLength = $false
+        }
+
+        # make sure there's a 'separator' row between the table header and the rows
+        $headerSeparator = $true
+        $divider = [string]::Join('',$rows[1])
+        if (($divider.toCharArray() | Get-Unique) -ne "-") {
+            $headerSeparator = $false
+        }
+
+        # input validation
+        if (!($uniformLength -and $headerSeparator)) {
+            Write-Output "Invalid input object"
+            return
+        }
+
+        # Process into array of hashtables
+        $headers = $rows[0]
+        $entries = $rows[2..($rows.length - 1)]
+        $output = @()
+
+        foreach ($row in $entries) {
+            $rowArray = $row.trim("|").split("|")
+            $rowOutput = New-Object -TypeName PSObject 
+            foreach ($idx in 0..($rowArray.length - 1)) {
+                Add-Member -InputObject $rowOutput -MemberType NoteProperty `
+                    -Name $headers[$idx] -Value $rowArray[$idx].trim()
+                #$rowOutput.add($headers[$idx], $rowArray[$idx].trim())
+            }
+            $output = $output + $rowOutput
+        }
+
+        return $output
+    }
 }
 
 function ConvertTo-MdTable {
@@ -62,27 +78,54 @@ function ConvertTo-MdTable {
         )]$InputObject
     )
     # TODO: Input Validation
-    $pTable = $InputObject | ForEach-Object {[PSCustomObject]$_} | Format-Table | Out-String
-    $rows = @($pTable.trim("`r`n") -split "`r`n|`n")
-    $headers = $rows[0]
-    $columnIndices = (@(0, ($headers.length)) + [regex]::Matches($headers, '(?<=\s)\S').Index) | Sort-Object
-    $numfields = $columnIndices.length - 1
-    $outStrings = @()
-    foreach ($idx in 0..($rows.length - 1)) {
-        $row = $rows[$idx]
-        $rowString = '|'
-        foreach ($fieldIDX in 1..$numfields) {
-            $start = $columnIndices[$fieldIDX-1]
-            $end = $columnIndices[$fieldIDX]
-            $length = $end - $start
-            $rowString += ' '
-            $rowString += $row.Substring($start, $length)
-            $rowString += ' |'
-        }
-        if ($idx -eq 1) {
-            $rowString = $rowString -replace " ","-"
-        }
-        $outStrings += $rowString
+
+    begin {
+        $all = @()
     }
-    return $outStrings
+
+    process {
+        foreach($input in $InputObject) {
+            $all += $input
+        }
+    }
+
+    end {
+        # ensure we're handling PSObjects regardless of input type
+        $all = $all | ForEach-Object {[PSCustomObject]$_}
+        # need to calculate the width of the table to prevent PS from truncating values
+        $props = ($all | Get-Member | Where MemberType -match "Property").name
+        # account for the extra spaces and punctuation
+        $length = 1 + 2*$props.length
+        foreach ($prop in $props) {
+            $fieldLen = 0
+            foreach ($item in $all) {
+                $fieldLen = [math]::Max($item.$prop.length, $fieldLen)
+            }
+            $length += $fieldLen
+        }
+        # leverage format-table to get us 90% of the way there
+        $pTable = $all | Format-Table -Property * -Expand Both -AutoSize | Out-String -Width $length
+        $rows = @($pTable.trim("`r`n") -split "`r`n|`n")
+        $headers = $rows[0]
+        $columnIndices = (@(0, ($headers.length)) + [regex]::Matches($headers, '(?<=\s)\S').Index) | Sort-Object
+        $numfields = $columnIndices.length - 1
+        $outStrings = @()
+        foreach ($idx in 0..($rows.length - 1)) {
+            $row = $rows[$idx]
+            $rowString = '|'
+            foreach ($fieldIDX in 1..$numfields) {
+                $start = $columnIndices[$fieldIDX-1]
+                $end = $columnIndices[$fieldIDX]
+                $length = $end - $start
+                $rowString += ' '
+                $rowString += $row.Substring($start, $length)
+                $rowString += ' |'
+            }
+            if ($idx -eq 1) {
+                $rowString = $rowString -replace " ","-"
+            }
+            $outStrings += $rowString
+        }
+        return $outStrings
+    }
 }
--- Test-Module.ps1
+++ Test-Module.ps1
@@ -1,11 +1,29 @@
 Write-Output "`r`nImporting Module...`r`n"
 Import-Module "./PSMarkdownTable.psm1"
 
+# To Table, As Parameter
 Write-Output "Testing conversion from PowerShell objects to Markdown Table:`r`n"
 $json_data = Get-Content "./test-data.json" | ConvertFrom-Json
 ConvertTo-MdTable -InputObject $json_data
 
+# To Table, As Pipeline
+Write-Output "`r`n`r`nTesting conversion when received from pipeline:`r`n"
+Get-Content "./test-data.json" | ConvertFrom-Json | ConvertTo-MdTable
+
+# From Table, As Parameter
 Write-Output "`r`n`r`nTesting conversion from Markdown Table to PowerShell objects:"
 $md_data = Get-Content "./test-data.md"
 # by default, PowerShell will 'flatten' the array of hashtables for display, so we do some formatting
-ConvertFrom-MdTable -InputObject $md_data | ForEach-Object {[PSCustomObject]$_} | Format-Table
+ConvertFrom-MdTable -InputObject $md_data 
+
+# From Table, As Pipeline
+Write-Output "`r`n`r`nTesting conversion when received from pipeline:`r`n"
+Get-Content "./test-data.md" | ConvertFrom-MdTable | Format-Table
+
+# From Table --> To Table
+Write-Output "`r`n`r`nTesting 'from table to table':`r`n"
+Get-Content "./test-data.md" | ConvertFrom-MdTable | ConvertTo-MdTable
+
+# To Table --> From Table
+Write-Output "`r`n`r`nTesting 'to table from table':`r`n"
+Get-Content "./test-data.json" | ConvertFrom-Json | ConvertTo-MdTable | ConvertFrom-MdTable | Format-Table
